c0f7f82fd2824a4f018b8a7c27256c26ce3d707a
amandaseara <amanda.seara@correo.ucu.edu.uy>
2022-06-05 20:21:31 -0300

    Ejercicio 1

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using CompAndDel.Pipes;
 using CompAndDel.Filters;
+using Path = System.IO.Path;
 
 namespace CompAndDel
 {
@@ -8,6 +9,19 @@ namespace CompAndDel
     {
         static void Main(string[] args)
         {
+            //Ej. 1:
+
+            PictureProvider pictureProvider = new PictureProvider();
+            IPicture picture = pictureProvider.GetPicture(@"luke.jpg");
+            IFilter greyscale = new FilterGreyscale();
+            IFilter negative = new FilterNegative();
+            IPipe pipeN = new PipeNull();
+            IPipe pipeNegative = new PipeSerial(negative, pipeN);
+            IPicture imageNegative = pipeNegative.Send(picture);
+            IPipe pipeGrey = new PipeSerial(greyscale, pipeNegative);
+            IPicture image = pipeGrey.Send(imageNegative);
+            PictureProvider provider = new PictureProvider();
+            provider.SavePicture(image, @"luke2.jpg");
         }
     }
 }
